283f76cd7a58105759c15c7fc1cd33be8bd3dc9b
v1.x pasas <alex09.ap19@gmail.com>
2023-10-25 19:20:06 +0300

    test

--- Dimitra_CRUD-main/backend/server.js
+++ Dimitra_CRUD-main/backend/server.js
@@ -1,23 +1,17 @@
 const express = require ("express");
+const app = express();
 const connection = require ("./connection");
 const cors = require ("cors");
-const user_routers = require("./routers/user_routers");
-const employeeRoutes = require("./routers/employeeRouter");
-
-
-const app = express();
 const port = 8000;
 
-//middleware 
+//middleware
 app.use(express.json());
 app.use(cors());
 
-// Use the routers
+const employeeRoutes = require("./routers/employeeRouter");
+
+
 app.use ("/", employeeRoutes);
-app.use("/", user_routers);
-
-
-
 app.listen (port, () => {
     console.log (`The server is working in port ${port}`)
 });
