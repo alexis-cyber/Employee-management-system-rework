77bc5fb1b79d8b1ecfa30da6ff28de940d0fd1f7
Wilkista-Wandolo <wilkywands@gmail.com>
2023-10-29 10:11:48 +0200

    connecting to Team database

--- Dimitra_CRUD-main/backend/server.js
+++ Dimitra_CRUD-main/backend/server.js
@@ -3,7 +3,7 @@ const connection = require ("./connection");
 const cors = require ("cors");
 const user_routers = require("./routers/user_routers");
 const employeeRoutes = require("./routers/employeeRouter");
-
+const path = require("path");
 
 const app = express();
 const port = 8000;
@@ -11,6 +11,7 @@ const port = 8000;
 //middleware 
 app.use(express.json());
 app.use(cors());
+// app.use("/images", express.static(path.join(--__dirname, images)))
 
 // Use the routers
 app.use ("/", employeeRoutes);
